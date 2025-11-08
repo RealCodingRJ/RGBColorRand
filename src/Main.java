7f587ae27de17431f5da96097063e8f65afed351
RYAN JAMES <realcoder2023@gmail.com>
2025-11-08 18:43:16 -0500

    added main code for color code

--- src/Main.java
+++ src/Main.java
@@ -19,28 +19,36 @@ void main() {
     frame.setPreferredSize(new Dimension(300, 900));
     frame.setSize(new Dimension(300, 600));
 
-    button.setBorder(BorderFactory.createEmptyBorder());
-    button.setBorderPainted( false );
+    JLabel lbl1 = new JLabel();
+    JLabel lbl2 = new JLabel();
+    JLabel lbl3 = new JLabel();
+
+    button.setBorderPainted(false);
     button.setFocusPainted(false);
 
-    frame.add(button);
-
-
-    var pane = (JPanel) frame.getContentPane();
+    var pane = frame.getContentPane();
     pane.add(button, BorderLayout.SOUTH);
+    frame.setLayout(new FlowLayout());
+
+    pane.add(lbl1);
+    pane.add(lbl2);
+    pane.add(lbl3);
 
     button.addActionListener(_ -> {
 
-        final int indexR = indexRand(1);
-        final int indexG = indexRand(2);
-        final int indexB = indexRand(3);
+        var indexR = indexRand(9);
+        var indexG = indexRand(5);
+        var indexB = indexRand(2);
 
+        System.out.println("Hello");
         frame.getContentPane().setBackground(new Color(indexR, indexG, indexB));
+
+        lbl1.setText("R: " + indexR);
+        lbl2.setText("G: " + indexG);
+        lbl3.setText("B: " + indexB);
     });
 
-    button.setPreferredSize(new Dimension(25 * 10, 20 + 10));
+    button.setPreferredSize(new Dimension(330, 20 + 10));
     frame.setVisible(true);
-
-
 
 }
