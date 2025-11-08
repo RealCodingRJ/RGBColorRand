b8fa81828e91bc7f2a3ee6000d61129244bbfd35
RYAN JAMES <realcoder2023@gmail.com>
2025-11-07 23:02:55 -0500

    added colors to color showing application

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 
+
+int indexRand (int x) {
+    return new Random().nextInt(x, 255);
+}
+
+
 void main() {
 
     JButton button = new JButton("Generate Color");
@@ -19,8 +25,18 @@ void main() {
 
     frame.add(button);
 
+
     var pane = (JPanel) frame.getContentPane();
     pane.add(button, BorderLayout.SOUTH);
+
+    button.addActionListener(_ -> {
+
+        final int indexR = indexRand(1);
+        final int indexG = indexRand(2);
+        final int indexB = indexRand(3);
+
+        frame.getContentPane().setBackground(new Color(indexR, indexG, indexB));
+    });
 
     button.setPreferredSize(new Dimension(25 * 10, 20 + 10));
     frame.setVisible(true);
